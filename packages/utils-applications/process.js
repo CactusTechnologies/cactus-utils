8fabf095394e2075b3543adbdd4d9ff955dbe5ba
Jorge Proaño <jorge@hiddennodeproblem.com>
2018-03-30 12:01:38 -0400

    Removed print Init and exposes PMX

--- packages/utils-applications/process.js
+++ packages/utils-applications/process.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const Config = require('config')
-const pmx = require('pmx')
 const moment = require('moment-timezone')
 const lo = require('lodash')
 const asyncExitHook = require('async-exit-hook')
@@ -15,16 +14,29 @@ Config.util.setModuleDefaults('pmx', {
   ports: true
 })
 
+/** @type {Object} PMX module */
+exports.pmx = require('pmx')
+
+/**
+ * Appends Listeners for:
+ *   - uncaughtException
+ *   - unhandledRejection
+ *   - process exit
+ * Initializes PMX and Logs Basic configuration data.
+ *
+ * @return {Promise}
+ */
+
 exports.init = () =>
   new Promise((resolve, reject) => {
     /* Enable PMX - Keymetrics */
-    pmx.init(Config.get('pmx'))
+    exports.pmx.init(Config.get('pmx'))
     /* Catch uncaughExeptions */
     process.prependListener(
       'uncaughtException',
       function customUncaughtExceptionListener (error) {
         log.fatal({ err: error }, `Undhandled Error: ${error.message}`)
-        pmx.notify(error)
+        exports.pmx.notify(error)
         process.nextTick(() => process.exit(1))
       }
     )
@@ -47,21 +59,22 @@ exports.init = () =>
       else log.warn(exitMessage)
     })
 
-    resolve()
-  })
+    log.info(`Initializing ${Config.get('appName')} v${Config.get('version')}`)
+    // prettier-ignore
+    log.info(`Configuring ${Config.get('appName')} for: ${Config.util.getEnv('HOSTNAME')} - ${Config.get('env')}`)
 
-exports.printInfo = (extras = {}) =>
-  new Promise((resolve, reject) => {
-    /* Log the configuration */
-    log.info(
-      {
-        env: Config.get('env'),
-        instance: Config.get('instance'),
-        logs: Config.get('logs.level'),
-        ...extras
-      },
-      `Initializing ${Config.get('appName')} v${Config.get('version')}`
-    )
+    log.info(`Log level: ${Config.get('logs.level')}`)
+
+    if (Config.get('logs.streams.pretty')) {
+      log.info('Logging style: pretty')
+    } else {
+      log.info('Logging style: bunyan')
+    }
+
+    // prettier-ignore
+    if (Config.get('logs.streams.cloudWatch')) {
+      log.info(`Sending Logs to AWS CloudWatch under: ${Config.get('logs.cloudWatch.logGroupName')} - ${Config.get('logs.cloudWatch.logStreamName')}`)
+    }
 
     resolve()
   })
