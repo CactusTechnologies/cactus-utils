e52490dae75b0a1364e83168bb0f59598bbf5dfd
Jorge Proaño <jorge@hiddennodeproblem.com>
2018-05-24 00:44:30 -0400

    Quick commit [Phobos2017]

--- packages/utils-applications/pm2.js
+++ packages/utils-applications/pm2.js
@@ -180,9 +180,7 @@ class Application {
 }
 
 fp.forEach(prop =>
-  Object.defineProperty(Application.prototype, prop, {
-    enumerable: true
-  })
+  Object.defineProperty(Application.prototype, prop, { enumerable: true })
 )(visibleKeys)
 
 module.exports = Application
