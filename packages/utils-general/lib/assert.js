525b9ea9153e554483a43441751fa95484f7281a
Jorge Proaño <jorge@hiddennodeproblem.com>
2018-08-02 02:19:15 -0400

    asert.checkDigit

--- packages/utils-general/lib/assert.js
+++ packages/utils-general/lib/assert.js
@@ -367,3 +367,33 @@ exports.notOkStatus = entry => fp.negate(exports.isOkStatus)(entry)
  * @alias module:@cactus-technologies/utils.assert.exists
  */
 exports.exists = path => require('fs').existsSync(path)
+
+/**
+ * Checks if the given input string against the Damm algorithm
+ *
+ * @param {String} input - Numeric String, probs from uuid.numeric()
+ *
+ * @return {Boolean}
+ *
+ * @category Integrity Validation
+ * @memberOf assert
+ * @alias module:@cactus-technologies/utils.assert.checkDigit
+ */
+
+exports.checkDigit = input => {
+  const damnTable = [
+    [0, 3, 1, 7, 5, 9, 8, 6, 4, 2],
+    [7, 0, 9, 2, 1, 5, 4, 8, 6, 3],
+    [4, 2, 0, 6, 8, 7, 1, 3, 5, 9],
+    [1, 7, 5, 0, 9, 8, 3, 4, 2, 6],
+    [6, 1, 2, 3, 0, 4, 5, 9, 7, 8],
+    [3, 6, 7, 4, 2, 0, 9, 5, 8, 1],
+    [5, 8, 6, 9, 7, 2, 0, 1, 3, 4],
+    [8, 9, 4, 5, 3, 6, 2, 0, 1, 7],
+    [9, 4, 3, 8, 6, 1, 7, 2, 0, 5],
+    [2, 5, 8, 1, 4, 3, 6, 7, 9, 0]
+  ]
+  let row = 0
+  for (let i = 0; i < input.length; i++) row = damnTable[row][input.charAt(i)]
+  return row.toString() === '0'
+}
