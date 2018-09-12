e277c56db1e6c4279e61f7e4a38c3c3a50e2392d
Jorge Proaño <jorge@hiddennodeproblem.com>
2018-09-12 17:02:53 -0400

    chore(scripts): Updated Package scripts.

--- scripts/docs.js
+++ scripts/docs.js
@@ -8,22 +8,22 @@ const tasks = [
     title: 'Fetching AUTHORS',
     task: () => shell('git log --format="%aN <%aE>" | sort -f | uniq > AUTHORS')
   },
-  {
-    title: 'Compiling Docs',
-    task: () => shell('projectz compile')
-  },
-  {
-    title: 'Add TODOs to the README',
-    task: () => shell('npm run todos -- --append')
-  },
+  // {
+  //   title: 'Compiling Docs',
+  //   task: () => shell('projectz compile')
+  // },
+  // {
+  //   title: 'Add TODOs to the README',
+  //   task: () => shell('npm run todos -- --append')
+  // },
   {
     title: 'Running modules Documentation',
     task: () => shell('lerna run docs')
   },
-  {
-    title: 'Compiling modules Docs',
-    task: () => shell('lerna exec --no-bail projectz -- "compile"')
-  },
+  // {
+  //   title: 'Compiling modules Docs',
+  //   task: () => shell('lerna exec --no-bail projectz -- "compile"')
+  // },
   {
     title: 'Adding Docs to git',
     task: () => {
