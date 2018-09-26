0a949644d24871571350c34fefc7a8d98ce8abb4
Jorge Proaño <jorge@hiddennodeproblem.com>
2018-09-26 16:46:08 -0400

    feat(eslint-config): Exporting custom react and module configs

---
+++ packages/eslint-config/module.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['@cactus-technologies'],
+  env: { node: true },
+  rules: { 'valid-jsdoc': 'warn' }
+}
---
+++ packages/eslint-config/react.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@cactus-technologies', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  plugins: ['react'],
+  env: {
+    browser: true
+  }
+}
